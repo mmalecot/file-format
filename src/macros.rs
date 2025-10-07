44cb2ce364d96693578e6c39e86550306e01bccf
Cody Laeder <codylaeder@gmail.com>
2025-10-07 16:55:48 -0700

    Add method of recovering name
    Permit converting name to a file format

--- src/macros.rs
+++ src/macros.rs
@@ -144,6 +144,31 @@ macro_rules! formats {
                     )*
                 }
             }
+
+            /// Converts the full name of a file format into the enum representation
+            ///
+            /// # Examples
+            ///
+            /// Basic usage:
+            ///
+            /// ```
+            /// use file_format::{FileFormat, Kind};
+            ///
+            /// let fmt = FileFormat::Mpeg12AudioLayer3;
+            /// let name = fmt.name();
+            /// assert_eq!(name, "MPEG-1/2 Audio Layer 3");
+            /// assert_eq!(FileFormat::from_name(name), Some(fmt));
+            /// ```
+            pub fn from_name(name: &str) -> Option<crate::FileFormat> {
+                const NAME: std::sync::OnceLock<std::collections::HashMap<&'static str,crate::FileFormat>> = std::sync::OnceLock::new();
+                NAME.get_or_init(|| -> std::collections::HashMap<&'static str,crate::FileFormat> {
+                    let mut map = std::collections::HashMap::new();
+                    $(
+                        map.insert($name, Self::$format);
+                    )*
+                    map
+                }).get(name).cloned()
+            }
         }
     };
 }
